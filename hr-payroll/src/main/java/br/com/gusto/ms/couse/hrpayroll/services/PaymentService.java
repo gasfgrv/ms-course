6cbc69d9e046f6810f1393997af7a8ce8aa16e68
Gustavo Almeida <gustavo.almeida@autbank.com.br>
2021-06-08 00:03:25 -0300

    implementação do OpenFeign no projeto hr-payroll para pegar os dados do trabalhador

--- hr-payroll/src/main/java/br/com/gusto/ms/couse/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/gusto/ms/couse/hrpayroll/services/PaymentService.java
@@ -1,14 +1,24 @@
 package br.com.gusto.ms.couse.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.gusto.ms.couse.hrpayroll.entities.Payment;
+import br.com.gusto.ms.couse.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
+	private final WorkerFeignClient client;
+
+	@Autowired
+	public PaymentService(WorkerFeignClient client) {
+		this.client = client;
+	}
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Jorge", 200.0, days);
+		var worker = client.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
