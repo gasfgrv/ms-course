efcf4cbca18f49923cbfec8c1b621dbea3896b10
Gustavo Silva <gusta-silva@pop-os.localdomain>
2021-06-07 21:08:44 -0300

    Projeto hr-worker

---
+++ hr-worker/src/test/java/br/com/gusto/ms/course/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gusto.ms.course.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
