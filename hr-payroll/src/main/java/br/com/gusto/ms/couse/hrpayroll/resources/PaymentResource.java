e2167638ead70c19c3f09f6f378ea946cdbe6a72
Gustavo Almeida <gustavo.almeida@autbank.com.br>
2021-06-14 22:53:33 -0300

    Circuit breaker com hystrix (NÃO USEM ESSA PORRA)

--- hr-payroll/src/main/java/br/com/gusto/ms/couse/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/gusto/ms/couse/hrpayroll/resources/PaymentResource.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.gusto.ms.couse.hrpayroll.entities.Payment;
 import br.com.gusto.ms.couse.hrpayroll.services.PaymentService;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentResource {
-	
+
 	private final PaymentService service;
 
 	@Autowired
@@ -21,9 +23,15 @@ public class PaymentResource {
 		this.service = service;
 	}
 
+	// O Hystrix já é um projeto depreciado!!! Não usem em nenhum projeto, uma
+	// alternativa é o Resilience4j
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable("workerId") long workerId, @PathVariable("days") int days) {
 		return ResponseEntity.ok(service.getPayment(workerId, days));
 	}
-	
+
+	public ResponseEntity<Payment> getPaymentFallback(long workerId, int days) {
+		return ResponseEntity.ok(new Payment("Error", 0.0, 0));
+	}
 }
